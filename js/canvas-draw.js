6610a285b554a00677d2fa1bb538b98ea3f5c748
ry-yuan <1094324110@qq.com>
2017-11-15 15:18:57 +0800

    [update] canvas tool

--- js/canvas-draw.js
+++ js/canvas-draw.js
@@ -7,33 +7,31 @@ window.onload = function () {
     var redBtn = document.getElementById('redBtn');
     var blueBtn = document.getElementById('blueBtn');
     var greenBtn = document.getElementById('greenBtn');
-
-    //颜色选框
-    var selectColor = document.getElementById('selectColor');
-
-    // 粗细选框
-    var selectLineWidth = document.getElementById('selectLineWidth');
-    selectColor.onchange = function () {
-        changeColor(this.value);
-    };
-    selectLineWidth.onchange = function(){
-        changeLineWidth(this.value);
-        console.log(this.value);
-    }
     //利用鼠标事件来控制绘画
     canvas.onmousedown = startDrawing;
     canvas.onmouseup = stopDrawing;
     canvas.onmouseout = stopDrawing;
     canvas.onmousemove = draw;
 
+    //颜色选框
+    var selectColor = document.getElementById('selectColor');
+    selectColor.onchange = function () {
+        changeColor(this.value);
+    };
+    // 粗细选框
+    var selectLineWidth = document.getElementById('selectLineWidth');
+    selectLineWidth.onchange = function () {
+        changeLineWidth(this.value);
+        console.log(this.value);
+    }
+
     //changeColor 改变颜色函数
     function changeColor(color, item) {
         //线条颜色
         context.strokeStyle = color;
-        // alert('hji');
     }
 
-    function changeLineWidth(size){
+    function changeLineWidth(size) {
         context.lineWidth = size;
     }
     //记录是否开始绘画，默认false
@@ -61,4 +59,13 @@ window.onload = function () {
     function stopDrawing() {
         isDrawing = false;
     }
+    // 清空画布
+    function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    var clear_canvas = document.getElementById('clearCanvas');
+    clear_canvas.onclick = function(){
+        clearCanvas();
+    }
+
 }
